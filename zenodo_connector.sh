6aed42614c5156cdca8f3ed05f5810e42306937a
Christos Sidiropoulos <dev@csidirop.de>
2025-03-19 15:43:56 +0100

    Add zenodo api access token via CLI
     - add plain access token
     - add path to file with access token
     - try to read a file called `token` if no token was set
    
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- zenodo_connector.sh
+++ zenodo_connector.sh
@@ -3,19 +3,17 @@
 # Zenodo Connector
 # 
 # This script is uses the Zenodo API to create, upload, discard and publish records.
-# Usage:  ./zenodo_connector.sh --mode=[init|upload|discard|publish] --record_id=[id] --file=[file]
+# Usage:  ./zenodo_connector.sh --mode=[init|upload|discard|publish] --record_id=[id] --file=[file] --access_token=[path/to/token|token]
 # Example:
-#   ./zenodo_connector.sh --mode=init
-#   ./zenodo_connector.sh --mode=upload --record_id=1234 --file=path/to/example.txt
-#   ./zenodo_connector.sh --mode=discard --record_id=1234
-#   ./zenodo_connector.sh --mode=publish --record_id=1234
+#   ./zenodo_connector.sh --mode=init --access_token=[path/to/token|123456token]
+#   ./zenodo_connector.sh --mode=upload --record_id=1234 --file=path/to/example.txt --access_token=[path/to/token|123456token]
+#   ./zenodo_connector.sh --mode=discard --record_id=1234 --access_token=[path/to/token|123456token]
+#   ./zenodo_connector.sh --mode=publish --record_id=1234 --access_token=[path/to/token|123456token]
 
 set -euo pipefail # exit on: error, undefined variable, pipefail
 
 # instance=https://zenodo.org
 instance=https://sandbox.zenodo.org
-# accessToken= #live
-accessToken= #sandbox
 
 # mode=init
 # mode=upload
@@ -37,6 +35,10 @@ for arg in "$@"; do
         file="${arg#*=}"
         shift
         ;;
+    -t=* | --access_token=*)
+        access_token="${arg#*=}"
+        shift
+        ;;
     *)
         if [ -z "$file" ]; then
             file="$arg"
@@ -44,11 +46,33 @@ for arg in "$@"; do
     esac
 done
 
+# Check if curl is installed:
+if ! command -v curl &>/dev/null; then
+    echo "Curl is not installed. Please install Curl."
+    exit 1
+fi
+
+# Check access token:
+if [ -n "$access_token" ]; then # check if access token is not empty
+    if [ -f "$access_token" ]; then # check if access token is a readable file
+        access_token=$(cat $access_token)
+        echo "Read access token from given file!"
+    else
+        echo "Access token provided!"
+    fi
+elif [ -f "token" ]; then # try to read access token from static file called 'token'
+    access_token=$(cat token)
+    echo "Read static access token!"
+else
+    echo "No access token provided!"
+    exit 1
+fi
+
 # Main logic:
 if [ "$mode" == "init" ]; then
     echo "Init ..."
     curl -X POST $instance/api/deposit/depositions \
-        -H "Authorization: Bearer "$accessToken \
+        -H "Authorization: Bearer "$access_token \
         -H "Content-Type: application/json" \
         -d '{}' > response.json
 
@@ -59,20 +83,20 @@ if [ "$mode" == "init" ]; then
 elif [ "$mode" == "discard" ]; then 
     echo "Discarding..."
     curl -X POST $instance/api/deposit/depositions/$record_id/actions/discard \
-        -H "Authorization: Bearer "$accessToken \
+        -H "Authorization: Bearer "$access_token \
         -H "Content-Type: application/json" \
         -d '{}'
     echo -e "Discarded record ID: $record_id \n"
 elif [ "$mode" == "upload" ]; then 
     echo "Uploading file..."
     filename=$(basename $file)
-    bucket_url=$(curl $instance/api/deposit/depositions/$record_id?access_token=$accessToken | jq -r '.links.bucket')
-    curl --progress-bar --upload-file $file $bucket_url/$filename?access_token=$accessToken
+    bucket_url=$(curl $instance/api/deposit/depositions/$record_id?access_token=$access_token | jq -r '.links.bucket')
+    curl --progress-bar --upload-file $file $bucket_url/$filename?access_token=$access_token
     echo -e "Uploaded file: $file \n"
 elif [ "$mode" == "publish" ]; then 
     echo "Publishing..."
     curl -X POST $instance/api/deposit/depositions/$record_id/actions/publish \
-        -H "Authorization: Bearer "$accessToken \
+        -H "Authorization: Bearer "$access_token \
         -H "Content-Type: application/json" \
         -d '{}'
     echo -e "Published record ID: $record_id \n"
